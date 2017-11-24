3dbec7f3c5d587be6bf8a8f44df78f66a8163fea
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2017-11-25 04:47:19 +0900

    いろいろ追加

--- slices.go
+++ slices.go
@@ -3,6 +3,8 @@ package slices
 import (
 	"reflect"
 	"errors"
+	"math/rand"
+	"bytes"
 )
 
 func sliceValue(slice interface{}) reflect.Value {
@@ -14,32 +16,208 @@ func sliceValue(slice interface{}) reflect.Value {
 	return value
 }
 
-func Find(slice interface{}, f func(int) bool) interface{} {
+// 指定した位置の要素を返す。indexが範囲外のときはdefaultValueを返す。
+func Fetch(slice interface{}, index int, defaultValue interface{}) interface{} {
+	src := sliceValue(slice)
+	if index < src.Len() {
+		return src.Index(index).Interface()
+	}
+	return defaultValue
+}
+
+// スライスの要素の数だけ関数fを繰り返し実行し、その繰り返しをn回続ける。
+func Cycle(slice interface{}, n int, f func(int) error) error {
 	src := sliceValue(slice)
 
 	length := src.Len()
-	for i := 0; i < length; i++ {
-		if f(i) {
-			return src.Index(i).Interface()
+	for i := 0; i < n; i++ {
+		for j := 0; j < length; j++ {
+			if e := f(j); e != nil {
+				return e
+			}
 		}
 	}
 
 	return nil
 }
 
-func Split(slice interface{}, f func(int) bool) (interface{}, interface{}) {
+// スライスの長さの最大値を返す。
+func MaxLength(slices ...interface{}) int {
+	max := 0
+	for _, slice := range slices {
+		if length := sliceValue(slice).Len(); length > max {
+			max = length
+		}
+	}
+	return max
+}
+
+// スライスの長さの最小値を返す。
+func MinLength(slices ...interface{}) int {
+	if len(slices) <= 0 {
+		return 0
+	}
+
+	min := sliceValue(slices[0]).Len()
+	for i := 1; i < len(slices); i++ {
+		if length := sliceValue(slices[i]).Len(); length < min {
+			min = length
+		}
+	}
+	return min
+}
+
+// 要素を結合した文字列を返す。
+func Join(slice interface{}, sep string, f func(int) string) string {
+	length := sliceValue(slice).Len()
+
+	if length <= 0 {
+		return ""
+	}
+
+	buf := bytes.NewBuffer(make([]byte, 0, (length + len(sep)) * 4 + 1))
+
+	length--
+	for i := 0; i < length; i++ {
+		buf.WriteString(f(i))
+		buf.WriteString(sep)
+	}
+
+	buf.WriteString(f(length))
+
+	return buf.String()
+}
+
+// スライスを複製する。
+func Clone(slice interface{}) interface{} {
+	src := sliceValue(slice)
+
+	dst := reflect.MakeSlice(src.Type(), src.Len(), src.Len())
+	reflect.Copy(dst, src)
+
+	return dst.Interface()
+}
+
+// 要素をランダムに入れ替える。
+func Shuffle(slice interface{}, r *rand.Rand) {
+	src := sliceValue(slice)
+
+	swap := reflect.Swapper(src.Interface())
+
+	length := src.Len()
+	for i := 0; i < length; i++ {
+		swap(i, r.Intn(i + 1))
+	}
+}
+
+// 要素を１つランダムに返す。
+func Sample(slice interface{}, r *rand.Rand) interface{} {
+	src := sliceValue(slice)
+	length := src.Len()
+	return src.Index(r.Intn(length)).Interface()
+}
+
+// 最もよく条件を満たす要素を返す。
+func Top(slice interface{}, f func(int, int) bool) interface{} {
+	src := sliceValue(slice)
+
+	length := src.Len()
+
+	if length <= 0 {
+		return reflect.Zero(src.Type().Elem()).Interface()
+	}
+
+	top := 0;
+	for i := 1; i < length; i++ {
+		if f(top, i) {
+			top = i
+		}
+	}
+
+	return src.Index(top).Interface()
+}
+
+// 条件を満たす最初の要素を返す。
+func Find(slice interface{}, f func(int) bool) (interface{}, bool) {
 	src := sliceValue(slice)
 
 	length := src.Len()
 	for i := 0; i < length; i++ {
 		if f(i) {
-			return src.Slice(0, i + 1).Interface(), src.Slice(i + 1, length).Interface()
+			return src.Index(i).Interface(), true
 		}
 	}
 
-	return slice, reflect.MakeSlice(src.Type(), 0, 0)
+	return reflect.Zero(src.Type().Elem()).Interface(), false
 }
 
+// 条件を満たす先頭部分と満たさない残りの部分を返す。
+func Span(slice interface{}, f func(int) bool) (interface{}, interface{}) {
+	src := sliceValue(slice)
+
+	length := src.Len()
+	for i := 0; i < length; i++ {
+		if !f(i) {
+			return src.Slice(0, i).Interface(), src.Slice(i, length).Interface()
+		}
+	}
+
+	return slice, reflect.MakeSlice(src.Type(), 0, 0).Interface()
+}
+
+// 条件を満たす先頭のスライスを返す。
+// 条件を満たさなかった時点で終了する。
+func TakeWhile(slice interface{}, f func(int) bool) interface{} {
+	src := sliceValue(slice)
+
+	length := src.Len()
+	for i := 0; i < length; i++ {
+		if !f(i) {
+			return src.Slice(0, i).Interface()
+		}
+	}
+
+	return slice
+}
+
+// 条件を満たす先頭の要素を除いていったスライス。
+// 条件を満たさなかった時点で終了する。
+func DropWhile(slice interface{}, f func(int) bool) interface{} {
+	src := sliceValue(slice)
+
+	length := src.Len()
+	for i := 0; i < length; i++ {
+		if !f(i) {
+			return src.Slice(i, length).Interface()
+		}
+	}
+
+	return reflect.MakeSlice(src.Type(), 0, 0).Interface()
+}
+
+// 重複を排除したコレクションを返す。
+// 入力スライスの順序に影響を与える。
+func Distinct(slice interface{}, f func(int, int) bool) interface{} {
+	src := sliceValue(slice)
+
+	swap := reflect.Swapper(src.Interface())
+
+	length := src.Len()
+	for i := 0; i < length; i++ {
+		for j := i + 1; j < length; {
+			if f(i, j) {
+				length--
+				swap(j, length)
+			} else {
+				j++
+			}
+		}
+	}
+
+	return src.Slice(0, length).Interface()
+}
+
+// 条件を満たす要素だけのスライスを返す。
 func Filter(slice interface{}, f func(int) bool) interface{} {
 	src := sliceValue(slice)
 
@@ -60,7 +238,9 @@ func Filter(slice interface{}, f func(int) bool) interface{} {
 	return dst.Slice(0, count).Interface()
 }
 
-func FilterFast(slice interface{}, f func(int) bool) interface{} {
+// 条件を満たす要素だけのスライスを返す。
+// Filter よりも高速だが、入力スライスの順序に影響を与える。
+func FastFilter(slice interface{}, f func(int) bool) interface{} {
 	src := sliceValue(slice)
 
 	swap := reflect.Swapper(slice)
@@ -77,6 +257,47 @@ func FilterFast(slice interface{}, f func(int) bool) interface{} {
 	return src.Slice(0, count).Interface()
 }
 
+// 条件を満たさない要素だけのスライスを返す。
+func FilterNot(slice interface{}, f func(int) bool) interface{} {
+	src := sliceValue(slice)
+
+	dst := reflect.MakeSlice(src.Type(), src.Len(), src.Len())
+	reflect.Copy(dst, src)
+
+	swap := reflect.Swapper(dst.Interface())
+
+	count := 0
+	length := src.Len()
+	for i := 0; i < length; i++ {
+		if !f(i) {
+			swap(count, i)
+			count++
+		}
+	}
+
+	return dst.Slice(0, count).Interface()
+}
+
+// 条件を満たさない要素だけのスライスを返す。
+// FilterNot よりも高速だが、入力スライスの順序に影響を与える。
+func FastFilterNot(slice interface{}, f func(int) bool) interface{} {
+	src := sliceValue(slice)
+
+	swap := reflect.Swapper(slice)
+
+	count := 0
+	length := src.Len()
+	for i := 0; i < length; i++ {
+		if !f(i) {
+			swap(count, i)
+			count++
+		}
+	}
+
+	return src.Slice(0, count).Interface()
+}
+
+// 条件を満たすスライスと満たさないスライスを返す。
 func Partition(slice interface{}, f func(int) bool) (interface{}, interface{}) {
 	src := sliceValue(slice)
 
@@ -97,7 +318,9 @@ func Partition(slice interface{}, f func(int) bool) (interface{}, interface{}) {
 	return dst.Slice(0, count).Interface(), dst.Slice(count, dst.Len()).Interface()
 }
 
-func PartitionFast(slice interface{}, f func(int) bool) (interface{}, interface{}) {
+// 条件を満たすスライスと満たさないスライスを返す。
+// Partition よりも高速だが、入力スライスの順序に影響を与える。
+func FastPartition(slice interface{}, f func(int) bool) (interface{}, interface{}) {
 	src := sliceValue(slice)
 
 	swap := reflect.Swapper(slice)
@@ -114,6 +337,7 @@ func PartitionFast(slice interface{}, f func(int) bool) (interface{}, interface{
 	return src.Slice(0, count).Interface(), src.Slice(count, src.Len()).Interface()
 }
 
+// 条件を満たす要素の数を返す。
 func Count(slice interface{}, f func(int) bool) (count int) {
 	src := sliceValue(slice)
 
@@ -127,11 +351,28 @@ func Count(slice interface{}, f func(int) bool) (count int) {
 	return count
 }
 
-func Contains(slice interface{}, f func(int) bool) bool {
-	return Index(slice, f) >= 0
+// １つでも条件を満たす要素が存在したらtrue。
+func Exists(slice interface{}, f func(int) bool) bool {
+	return IndexWhere(slice, f) >= 0
 }
 
-func Index(slice interface{}, f func(int) bool) int {
+// 全ての要素が条件を満たすとき、true。
+func ForAll(slice interface{}, f func(int) bool) bool {
+	rs := sliceValue(slice)
+
+	length := rs.Len()
+	for i := 0; i < length; i++ {
+		if !f(i) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// 最初に条件を満たした要素の位置を返す。
+// 無い場合は-1が返る。
+func IndexWhere(slice interface{}, f func(int) bool) int {
 	rs := sliceValue(slice)
 
 	length := rs.Len()
@@ -144,7 +385,9 @@ func Index(slice interface{}, f func(int) bool) int {
 	return -1
 }
 
-func LastIndex(slice interface{}, f func(int) bool) int {
+// 最後に条件を満たした要素の位置を返す。
+// 無い場合は-1が返る。
+func LastIndexWhere(slice interface{}, f func(int) bool) int {
 	rs := sliceValue(slice)
 
 	length := rs.Len()
@@ -156,4 +399,25 @@ func LastIndex(slice interface{}, f func(int) bool) int {
 
 	return -1
 }
+
+// 最もよく条件を満たす要素の位置を返す。
+func TopIndex(slice interface{}, f func(int, int) bool) int {
+	src := sliceValue(slice)
+
+	length := src.Len()
+
+	if length <= 0 {
+		return -1
+	}
+
+	top := 0;
+	for i := 1; i < length; i++ {
+		if f(top, i) {
+			top = i
+		}
+	}
+
+	return top
+}
+
 
--- slices_test.go
+++ slices_test.go
@@ -2,10 +2,649 @@ package slices
 
 import (
 	"testing"
+	"errors"
+	"strconv"
 	"reflect"
 )
 
-func BenchmarkFind_Basic(b *testing.B) {
+func assert(t *testing.T, expected, actual interface{}) {
+	if expected == actual {
+		return
+	}
+
+	t.Errorf("expected: <%v>, but was: <%v>", expected, actual)
+}
+
+func TestFetch(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := Fetch(input, 1, 100)
+
+	assert(t, 2, output.(int))
+
+	input = []int{1, 2, 3}
+
+	output = Fetch(input, 3, 100)
+
+	assert(t, 100, output.(int))
+}
+
+func TestCycle(t *testing.T) {
+	input := []int{1, 2, 3}
+	output := []int{}
+
+	e := Cycle(input, 2, func(i int) error {
+		output = append(output, input[i])
+		return nil
+	})
+
+	assert(t, nil, e)
+	assert(t, 6, len(output))
+	assert(t, 1, output[0])
+	assert(t, 2, output[1])
+	assert(t, 3, output[2])
+	assert(t, 1, output[3])
+	assert(t, 2, output[4])
+	assert(t, 3, output[5])
+
+	input = []int{1, 2, 3}
+	output = []int{}
+	dummy := errors.New("")
+
+	e = Cycle(input, 2, func(i int) error {
+		if i == 2 {
+			return dummy
+		}
+
+		output = append(output, input[i])
+		return nil
+	})
+
+	assert(t, dummy, e)
+	assert(t, 2, len(output))
+	assert(t, 1, output[0])
+	assert(t, 2, output[1])
+}
+
+func TestMaxLength(t *testing.T) {
+	assert(t, 2, MaxLength([]int{}, []int{1}, []int{1, 2}))
+	assert(t, 2, MaxLength([]int{1}, []int{1, 2}, []int{}))
+	assert(t, 2, MaxLength([]int{1, 2}, []int{}, []int{1}))
+	assert(t, 0, MaxLength([]int{}, []int{}, []int{}))
+	assert(t, 1, MaxLength([]int{1}, []int{1}, []int{1}))
+	assert(t, 1, MaxLength([]int{1}))
+	assert(t, 0, MaxLength())
+}
+
+func TestMinLength(t *testing.T) {
+	assert(t, 0, MinLength([]int{}, []int{1}, []int{1, 2}))
+	assert(t, 0, MinLength([]int{1}, []int{1, 2}, []int{}))
+	assert(t, 0, MinLength([]int{1, 2}, []int{}, []int{1}))
+	assert(t, 0, MinLength([]int{}, []int{}, []int{}))
+	assert(t, 1, MinLength([]int{1}, []int{1}, []int{1}))
+	assert(t, 1, MinLength([]int{1}))
+	assert(t, 0, MinLength())
+}
+
+func TestJoin(t *testing.T) {
+	input := []int{1, 2, 3}
+	output := Join(input, ", ", func(i int) string {
+		return strconv.Itoa(input[i])
+	})
+	assert(t, "1, 2, 3", output)
+
+	input = []int{1}
+	output = Join(input, ", ", func(i int) string {
+		return strconv.Itoa(input[i])
+	})
+	assert(t, "1", output)
+
+	input = []int{}
+	output = Join(input, ", ", func(i int) string {
+		return strconv.Itoa(input[i])
+	})
+	assert(t, "", output)
+}
+
+func TestClone(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := Clone(input)
+
+	input[0] = 100;
+	input[1] = 200;
+	input[2] = 300;
+
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+	assert(t, 3, output.([]int)[2])
+}
+
+func TestTop(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := Top(input, func(i, j int) bool {
+		return input[i] < input[j]
+	})
+
+	assert(t, 3, output.(int))
+
+	input = []int{1, 2, 3}
+
+	output = Top(input, func(i, j int) bool {
+		return input[i] > input[j]
+	})
+
+	assert(t, 1, output.(int))
+}
+
+func TestFind(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output, ok := Find(input, func(i int) bool {
+		return input[i] == 1
+	})
+
+	assert(t, true, ok)
+	assert(t, 1, output.(int))
+
+	input = []int{1, 2, 3}
+
+	output, ok = Find(input, func(i int) bool {
+		return input[i] == 3
+	})
+
+	assert(t, true, ok)
+	assert(t, 3, output.(int))
+
+	output, ok = Find(input, func(i int) bool {
+		return input[i] == 4
+	})
+
+	assert(t, false, ok)
+	assert(t, 0, output.(int))
+}
+
+func TestSpan(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output1, output2 := Span(input, func(i int) bool {
+		return input[i] <= 2
+	})
+
+	assert(t, 2, len(output1.([]int)))
+	assert(t, 1, output1.([]int)[0])
+	assert(t, 2, output1.([]int)[1])
+	assert(t, 1, len(output2.([]int)))
+	assert(t, 3, output2.([]int)[0])
+
+	output1, output2 = Span(input, func(i int) bool {
+		return input[i] <= 0
+	})
+
+	assert(t, 0, len(output1.([]int)))
+	assert(t, 3, len(output2.([]int)))
+	assert(t, 1, output2.([]int)[0])
+	assert(t, 2, output2.([]int)[1])
+	assert(t, 3, output2.([]int)[2])
+
+	output1, output2 = Span(input, func(i int) bool {
+		return input[i] <= 3
+	})
+
+	assert(t, 3, len(output1.([]int)))
+	assert(t, 1, output1.([]int)[0])
+	assert(t, 2, output1.([]int)[1])
+	assert(t, 3, output1.([]int)[2])
+	assert(t, 0, len(output2.([]int)))
+}
+
+func TestTakeWhile(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := TakeWhile(input, func(i int) bool {
+		return input[i] <= 2
+	})
+
+	assert(t, 2, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+
+	output = TakeWhile(input, func(i int) bool {
+		return input[i] <= 0
+	})
+
+	assert(t, 0, len(output.([]int)))
+
+	output = TakeWhile(input, func(i int) bool {
+		return input[i] <= 3
+	})
+
+	assert(t, 3, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+	assert(t, 3, output.([]int)[2])
+}
+
+func TestDropWhile(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := DropWhile(input, func(i int) bool {
+		return input[i] <= 2
+	})
+
+	assert(t, 1, len(output.([]int)))
+	assert(t, 3, output.([]int)[0])
+
+	output = DropWhile(input, func(i int) bool {
+		return input[i] <= 0
+	})
+
+	assert(t, 3, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+	assert(t, 3, output.([]int)[2])
+
+	output = DropWhile(input, func(i int) bool {
+		return input[i] <= 3
+	})
+
+	assert(t, 0, len(output.([]int)))
+}
+
+func TestDistinct(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := Distinct(input, func(i, j int) bool {
+		return input[i] == input[j]
+	})
+
+	assert(t, 3, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+	assert(t, 3, output.([]int)[2])
+
+	input = []int{1, 1, 1}
+
+	output = Distinct(input, func(i, j int) bool {
+		return input[i] == input[j]
+	})
+
+	assert(t, 1, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+
+	input = []int{1, 2, 1}
+
+	output = Distinct(input, func(i, j int) bool {
+		return input[i] == input[j]
+	})
+
+	assert(t, 2, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+
+	input = []int{1, 2, 2}
+
+	output = Distinct(input, func(i, j int) bool {
+		return input[i] == input[j]
+	})
+
+	assert(t, 2, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+
+	input = []int{1, 1, 2}
+
+	output = Distinct(input, func(i, j int) bool {
+		return input[i] == input[j]
+	})
+
+	assert(t, 2, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+}
+
+func TestFilter(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := Filter(input, func(i int) bool {
+		return input[i] != 2
+	})
+
+	assert(t, 2, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 3, output.([]int)[1])
+
+	input = []int{1, 2, 3}
+
+	output = Filter(input, func(i int) bool {
+		return true
+	})
+
+	assert(t, 3, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+	assert(t, 3, output.([]int)[2])
+
+	input = []int{1, 2, 3}
+
+	output = Filter(input, func(i int) bool {
+		return false
+	})
+
+	assert(t, 0, len(output.([]int)))
+}
+
+func TestFastFilter(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := FastFilter(input, func(i int) bool {
+		return input[i] != 2
+	})
+
+	assert(t, 2, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 3, output.([]int)[1])
+
+	input = []int{1, 2, 3}
+
+	output = FastFilter(input, func(i int) bool {
+		return true
+	})
+
+	assert(t, 3, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+	assert(t, 3, output.([]int)[2])
+
+	input = []int{1, 2, 3}
+
+	output = FastFilter(input, func(i int) bool {
+		return false
+	})
+
+	assert(t, 0, len(output.([]int)))
+}
+
+func TestFilterNot(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := FilterNot(input, func(i int) bool {
+		return input[i] == 2
+	})
+
+	assert(t, 2, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 3, output.([]int)[1])
+
+	input = []int{1, 2, 3}
+
+	output = FilterNot(input, func(i int) bool {
+		return false
+	})
+
+	assert(t, 3, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+	assert(t, 3, output.([]int)[2])
+
+	input = []int{1, 2, 3}
+
+	output = FilterNot(input, func(i int) bool {
+		return true
+	})
+
+	assert(t, 0, len(output.([]int)))
+}
+
+func TestFastFilterNot(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := FastFilterNot(input, func(i int) bool {
+		return input[i] == 2
+	})
+
+	assert(t, 2, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 3, output.([]int)[1])
+
+	input = []int{1, 2, 3}
+
+	output = FastFilterNot(input, func(i int) bool {
+		return false
+	})
+
+	assert(t, 3, len(output.([]int)))
+	assert(t, 1, output.([]int)[0])
+	assert(t, 2, output.([]int)[1])
+	assert(t, 3, output.([]int)[2])
+
+	input = []int{1, 2, 3}
+
+	output = FastFilterNot(input, func(i int) bool {
+		return true
+	})
+
+	assert(t, 0, len(output.([]int)))
+}
+
+func TestPartition(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output1, output2 := Partition(input, func(i int) bool {
+		return input[i] == 2
+	})
+
+	assert(t, 1, len(output1.([]int)))
+	assert(t, 2, output1.([]int)[0])
+	assert(t, 2, len(output2.([]int)))
+	assert(t, 1, output2.([]int)[0])
+	assert(t, 3, output2.([]int)[1])
+
+	input = []int{1, 2, 3}
+
+	output1, output2 = Partition(input, func(i int) bool {
+		return true
+	})
+
+	assert(t, 3, len(output1.([]int)))
+	assert(t, 1, output1.([]int)[0])
+	assert(t, 2, output1.([]int)[1])
+	assert(t, 3, output1.([]int)[2])
+	assert(t, 0, len(output2.([]int)))
+
+	input = []int{1, 2, 3}
+
+	output1, output2 = Partition(input, func(i int) bool {
+		return false
+	})
+
+	assert(t, 0, len(output1.([]int)))
+	assert(t, 3, len(output2.([]int)))
+	assert(t, 1, output2.([]int)[0])
+	assert(t, 2, output2.([]int)[1])
+	assert(t, 3, output2.([]int)[2])
+}
+
+func TestFastPartition(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output1, output2 := FastPartition(input, func(i int) bool {
+		return input[i] == 2
+	})
+
+	assert(t, 1, len(output1.([]int)))
+	assert(t, 2, output1.([]int)[0])
+	assert(t, 2, len(output2.([]int)))
+	assert(t, 1, output2.([]int)[0])
+	assert(t, 3, output2.([]int)[1])
+
+	input = []int{1, 2, 3}
+
+	output1, output2 = FastPartition(input, func(i int) bool {
+		return true
+	})
+
+	assert(t, 3, len(output1.([]int)))
+	assert(t, 1, output1.([]int)[0])
+	assert(t, 2, output1.([]int)[1])
+	assert(t, 3, output1.([]int)[2])
+	assert(t, 0, len(output2.([]int)))
+
+	input = []int{1, 2, 3}
+
+	output1, output2 = FastPartition(input, func(i int) bool {
+		return false
+	})
+
+	assert(t, 0, len(output1.([]int)))
+	assert(t, 3, len(output2.([]int)))
+	assert(t, 1, output2.([]int)[0])
+	assert(t, 2, output2.([]int)[1])
+	assert(t, 3, output2.([]int)[2])
+}
+
+func TestCount(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := Count(input, func(i int) bool {
+		return input[i] == 2
+	})
+
+	assert(t, 1, output)
+
+	input = []int{1, 2, 3}
+
+	output = Count(input, func(i int) bool {
+		return true
+	})
+
+	assert(t, 3, output)
+
+	input = []int{1, 2, 3}
+
+	output = Count(input, func(i int) bool {
+		return false
+	})
+
+	assert(t, 0, output)
+}
+
+func TestExists(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := Exists(input, func(i int) bool {
+		return input[i] == 1
+	})
+
+	assert(t, true, output)
+
+	input = []int{1, 2, 3}
+
+	output = Exists(input, func(i int) bool {
+		return input[i] == 3
+	})
+
+	assert(t, true, output)
+
+	input = []int{1, 2, 3}
+
+	output = Exists(input, func(i int) bool {
+		return input[i] == 4
+	})
+
+	assert(t, false, output)
+}
+
+func TestForAll(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := ForAll(input, func(i int) bool {
+		return true
+	})
+
+	assert(t, true, output)
+
+	input = []int{1, 2, 3}
+
+	output = ForAll(input, func(i int) bool {
+		return input[i] <= 2
+	})
+
+	assert(t, false, output)
+}
+
+func TestIndexWhere(t *testing.T) {
+	input := []int{1, 1, 3}
+
+	output := IndexWhere(input, func(i int) bool {
+		return input[i] == 1
+	})
+
+	assert(t, 0, output)
+
+	input = []int{1, 2, 3}
+
+	output = IndexWhere(input, func(i int) bool {
+		return input[i] == 3
+	})
+
+	assert(t, 2, output)
+
+	input = []int{1, 2, 3}
+
+	output = IndexWhere(input, func(i int) bool {
+		return input[i] == 4
+	})
+
+	assert(t, -1, output)
+}
+
+func TestLastIndexWhere(t *testing.T) {
+	input := []int{1, 1, 3}
+
+	output := LastIndexWhere(input, func(i int) bool {
+		return input[i] == 1
+	})
+
+	assert(t, 1, output)
+
+	input = []int{1, 2, 3}
+
+	output = LastIndexWhere(input, func(i int) bool {
+		return input[i] == 3
+	})
+
+	assert(t, 2, output)
+
+	input = []int{1, 2, 3}
+
+	output = LastIndexWhere(input, func(i int) bool {
+		return input[i] == 4
+	})
+
+	assert(t, -1, output)
+}
+
+func TestTopIndex(t *testing.T) {
+	input := []int{1, 2, 3}
+
+	output := TopIndex(input, func(i, j int) bool {
+		return input[i] < input[j]
+	})
+
+	assert(t, 2, output)
+
+	input = []int{1, 2, 3}
+
+	output = TopIndex(input, func(i, j int) bool {
+		return input[i] > input[j]
+	})
+
+	assert(t, 0, output)
+}
+
+func BenchmarkFind_Muscle(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -15,13 +654,13 @@ func BenchmarkFind_Basic(b *testing.B) {
 
 	b.ResetTimer()
 
-	func() int {
+	func() (int, bool) {
 		for _, v := range slice {
 			if v == n {
-				return v
+				return v, true
 			}
 		}
-		return 0
+		return 0, false
 	}()
 }
 
@@ -33,13 +672,13 @@ func BenchmarkFind_BasicInterface(b *testing.B) {
 
 	n := len(slice) / 2
 
-	find := func(f func(interface{}) bool) interface{} {
+	find := func(f func(interface{}) bool) (interface{}, bool) {
 		for _, v := range slice {
 			if f(v) {
-				return v
+				return v, true
 			}
 		}
-		return 0
+		return 0, false
 	}
 
 	b.ResetTimer()
@@ -64,7 +703,7 @@ func BenchmarkFind(b *testing.B) {
 	})
 }
 
-func BenchmarkSplit_Basic(b *testing.B) {
+func BenchmarkSpan_Muscle(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -76,7 +715,7 @@ func BenchmarkSplit_Basic(b *testing.B) {
 
 	func() ([]int, []int) {
 		for i, v := range slice {
-			if v == n {
+			if !(v <= n) {
 				return slice[0:i + 1], slice[i + 1:]
 			}
 		}
@@ -84,7 +723,7 @@ func BenchmarkSplit_Basic(b *testing.B) {
 	}()
 }
 
-func BenchmarkSplit(b *testing.B) {
+func BenchmarkSpan(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -94,12 +733,12 @@ func BenchmarkSplit(b *testing.B) {
 
 	b.ResetTimer()
 
-	Split(slice, func(i int) bool {
-		return slice[i] == n
+	Span(slice, func(i int) bool {
+		return slice[i] <= n
 	})
 }
 
-func BenchmarkFilter_Basic(b *testing.B) {
+func BenchmarkFilter_Muscle(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -158,7 +797,7 @@ func BenchmarkFilter(b *testing.B) {
 	})
 }
 
-func BenchmarkFilter_Fast(b *testing.B) {
+func BenchmarkFastFilter(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -166,12 +805,12 @@ func BenchmarkFilter_Fast(b *testing.B) {
 
 	b.ResetTimer()
 
-	FilterFast(slice, func(i int) bool {
+	FastFilter(slice, func(i int) bool {
 		return slice[i] % 2 == 0
 	})
 }
 
-func BenchmarkPartition_Basic(b *testing.B) {
+func BenchmarkPartition_Muscle(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -236,7 +875,7 @@ func BenchmarkPartition(b *testing.B) {
 	})
 }
 
-func BenchmarkPartition_Fast(b *testing.B) {
+func BenchmarkFastPartition(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -244,12 +883,12 @@ func BenchmarkPartition_Fast(b *testing.B) {
 
 	b.ResetTimer()
 
-	PartitionFast(slice, func(i int) bool {
+	FastPartition(slice, func(i int) bool {
 		return slice[i] % 2 == 0
 	})
 }
 
-func BenchmarkCount_Basic(b *testing.B) {
+func BenchmarkCount_Muscle(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -281,7 +920,7 @@ func BenchmarkCount(b *testing.B) {
 	})
 }
 
-func BenchmarkContains_Basic(b *testing.B) {
+func BenchmarkExists_Muscle(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -301,7 +940,7 @@ func BenchmarkContains_Basic(b *testing.B) {
 	}()
 }
 
-func BenchmarkContains(b *testing.B) {
+func BenchmarkExists(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -311,12 +950,12 @@ func BenchmarkContains(b *testing.B) {
 
 	b.ResetTimer()
 
-	Contains(slice, func(i int) bool {
+	Exists(slice, func(i int) bool {
 		return slice[i] == n
 	})
 }
 
-func BenchmarkIndex_Basic(b *testing.B) {
+func BenchmarkIndexWhere_Muscle(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -336,7 +975,7 @@ func BenchmarkIndex_Basic(b *testing.B) {
 	}()
 }
 
-func BenchmarkIndex(b *testing.B) {
+func BenchmarkIndexWhere(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -346,12 +985,12 @@ func BenchmarkIndex(b *testing.B) {
 
 	b.ResetTimer()
 
-	Index(slice, func(i int) bool {
+	IndexWhere(slice, func(i int) bool {
 		return slice[i] == n
 	})
 }
 
-func BenchmarkLastIndex_Basic(b *testing.B) {
+func BenchmarkLastIndexWhere_Muscle(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -371,7 +1010,7 @@ func BenchmarkLastIndex_Basic(b *testing.B) {
 	}()
 }
 
-func BenchmarkLastIndex(b *testing.B) {
+func BenchmarkLastIndexWhere(b *testing.B) {
 	slice := make([]int, b.N)
 	for i := range slice {
 		slice[i] = i
@@ -381,7 +1020,7 @@ func BenchmarkLastIndex(b *testing.B) {
 
 	b.ResetTimer()
 
-	LastIndex(slice, func(i int) bool {
+	LastIndexWhere(slice, func(i int) bool {
 		return slice[i] == n
 	})
 }
